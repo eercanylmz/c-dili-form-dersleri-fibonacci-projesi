f9c95ea306a55a7923f743544800202b4bfbe041
yilma <yilma@ERCANMONSTER>
2022-11-07 01:53:51 +0300

    Proje dosyası ekle.

---
+++ c dılı form derslerıfıboaccı projesı/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_dılı_form_derslerıfıboaccı_projesı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1 = 1;
+            int number2 = 1;
+            int number3;
+            listBox1.Items.Add(number1);
+            listBox1.Items.Add(number2);
+            for (int i = 1; i <= 8; i++)
+            {
+                number3 = number1 + number2;
+                number1 = number2;
+                number2 = number3;
+                listBox1.Items.Add(number3);
+            }
+
+        }
+    }
+}
---
+++ c dılı form derslerıfıboaccı projesı/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_dılı_form_derslerıfıboaccı_projesı
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
